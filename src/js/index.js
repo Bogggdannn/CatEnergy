a61b9cea76345e37377256b7e072e4296a375c53
Bogggdannn <b.grishin1@yandex.ru>
2024-01-16 20:47:13 +0500

    написали функции для создания слайдера и его перемещения

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,29 @@ window.addEventListener("DOMContentLoaded", () => {
 @param  {Number} right ширина свободного пространства
 */
     function moveToggle(newLeft, right) {
-
+        if (newLeft < 0) {
+            newLeft = 0
+        }
+        if (newLeft > right) {
+            newLeft = right
+        }
+        let newWidth = Math.round(newLeft / right * 100)
+        imageBefore.style.width = `calc(${100 - newWidth}% + 40px)`
+        imageAfter.style.width = 100 - newWidth + "%"
+        if (newWidth == 0) {
+            imageBefore.style.width = newWidth + "%"
+            imageAfter.style.width = `calc(${100 - newWidth}% + 40px)`
+        }
+        sliderToggle.style.left = `${newWidth}%`
+        console.log(imageAfter)
     }
+    /**
+создаем slider
+*/
+    function initSlider() {
+        widthContainer = document.querySelector(".container").offsetWidth
+        console.log(widthContainer)
+    }
+    initSlider()
+    window.onresize = initSlider()
 })
