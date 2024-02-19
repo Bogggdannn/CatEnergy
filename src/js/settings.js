e5bae0261a8c27d0d7ad32b91cc065009d8e6e11
Bogggdannn <b.grishin1@yandex.ru>
2024-02-19 20:46:24 +0500

    сделали header, начали кота

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,14 +1,44 @@
-window.addEventListener("DOMContentLoaded",() => {
+const templatesNav = `            <img class="animate" src="./img/logo-desktop.png" alt="logo">
+<ul class="header-list">
+    <li class="header-list__item index cursor-pointer">
+        <a href="./index.html">Главная</a>
+    </li>
+    <li class="header-list__item catalog cursor-pointer">
+        <a href="./catalog.html">Каталог продукции</a>
+    </li>
+    <li class="header-list__item form cursor-pointer">
+        <a href="./form.html">Подбор программы</a>
+    </li>
+</ul>
+<div class="hamburger hamburger--collapse">
+    <div class="hamburger-box">
+      <div class="hamburger-inner"></div>
+    </div>
+  </div>`
+const templatesNavMobile = `<ul class="header-list__mobile">
+<li class="header-list__item index cursor-pointer">
+    <a href="./index.html">Главная</a>
+</li>
+<li class="header-list__item catalog cursor-pointer">
+    <a href="./catalog.html">Каталог продукции</a>
+</li>
+<li class="header-list__item form cursor-pointer">
+    <a href="./form.html">Подбор программы</a>
+</li>
+</ul>`
+window.addEventListener("DOMContentLoaded", () => {
     let widthContainer = 0
     const footerLogo = document.querySelector(".footer-academy img")
-    function getWidth(){
+    function getWidth() {
         widthContainer = document.querySelector("body").offsetWidth
         console.log(window.location.href)
-        if(widthContainer > 768){
-            footerLogo.src = window.location.href + "img/logo-footer.png"
-        }
-        else{
-            footerLogo.src = window.location.href + "img/htmlAcademyMobile.png"
+        if (footerLogo) {
+            if (widthContainer > 768) {
+                footerLogo.src = window.location.href + "img/logo-footer.png"
+            }
+            else {
+                footerLogo.src = window.location.href + "img/htmlAcademyMobile.png"
+            }
         }
     }
     getWidth()
@@ -17,17 +47,31 @@ window.addEventListener("DOMContentLoaded",() => {
     let pathName = window.location.pathname
     let html = document.querySelector("html")
     let mobileNav = document.querySelector(".mobile-nav")
+    let desktopNav = document.querySelector(".desktop-nav")
+    mobileNav.innerHTML = templatesNavMobile
+    desktopNav.innerHTML = templatesNav
     let hamburger = document.querySelector(".hamburger")
-    hamburger.onclick = function(){
+    hamburger.onclick = function () {
         mobileNav.classList.toggle("is-active")
         hamburger.classList.toggle("is-active")
-        if(hamburger.classList.contains("is-active")){
+        if (hamburger.classList.contains("is-active")) {
             html.style.overflow = "hidden"
         }
-        else{
+        else {
             html.style.overflow = ""
         }
-        
     }
-    console.log(window.location)
+    if(pathName.includes("index")){
+        desktopNav.querySelector(".index").classList.add("header-list__item--active")
+        mobileNav.querySelector(".index").classList.add("header-list__item--active")
+    }
+    else if(pathName.includes("form")){
+        console.log(desktopNav.querySelector(".form"))
+        desktopNav.querySelector(".form").classList.add("header-list__item--active")
+        mobileNav.querySelector(".form").classList.add("header-list__item--active")
+    }
+    else if(pathName.includes("catalog")) {
+        desktopNav.querySelector(".catalog").classList.add("header-list__item--active")
+        mobileNav.querySelector(".catalog").classList.add("header-list__item--active")
+    }
 })
